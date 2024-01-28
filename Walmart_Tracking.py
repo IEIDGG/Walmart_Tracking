bf4fc53e1848693d6dd52dd177c9f840d6f49345
IEID <alertspoiler00.93@gmail.com>
2024-01-27 19:46:40 -0500

    Update Walmart_Tracking.py

--- Walmart_Tracking.py
+++ Walmart_Tracking.py
@@ -4,66 +4,71 @@ from bs4 import BeautifulSoup
 import csv
 import datetime
 
+
 def extract_details(email_html):
     try:
         soup = BeautifulSoup(email_html, 'html.parser')
 
         links = soup.find_all('a', href=True, string=True)
-        try:
-            tracking_numbers = [link.string for link in links if
-                                'tracking number' in link.find_previous('div').text.lower()]
-        except AttributeError:
-            print('tracking number error 1')
-            tracking_numbers = []
-        if not tracking_numbers:
-            try:
-                tracking_numbers = [link.string for link in links if
-                                    'tracking number' in link.find_previous('span').text.lower()]
-            except AttributeError:
-                print('tracking number error 2')
-                tracking_numbers = []
 
-        order_date_div = soup.find('div', string=lambda text: text and 'order date' in text.lower())
-        order_date = order_date_div.get_text(strip=True).split(': ')[1] if order_date_div else ''
+        def tracking_finder():
+            tracking = [link.string for link in links if
+                                    'tracking number' in link.find_previous('div').text.lower()]
+            if not tracking:
 
-        try:
-            order_number_a = soup.find('div', string=lambda text: text and 'order number' in text.lower())
-            order_number = order_number_a.get_text(strip=True).split(': ')[1] if order_number_a else ''
-        except AttributeError:
-            print('order number error 1')
-            order_number = []
-        if not order_number:
-            try:
+                print('Tracking - Moving to method 2')
+
+                tracking_element = [link.string for link in soup.findAll('a', style='font-family:helvetica', target="_blank", href=True, rel=False)]
+                if 'Help Center' in tracking_element:
+                    tracking_element.remove('Help Center')
+                tracking = tracking_element
+
+            print(tracking)
+
+            return tracking
+
+        def order_date_finder():
+            order_date_div = soup.find('div', string=lambda text: text and 'order date' in text.lower())
+            order_date = order_date_div.get_text(strip=True).split(': ')[1] if order_date_div else ''
+
+            return order_date
+
+        def order_number_finder():
+            order_number_div = soup.find('div', string=lambda text: text and 'order number' in text.lower())
+            order_number = order_number_div.get_text(strip=True).split(': ')[1] if order_number_div else ''
+
+            if not order_number:
                 order_number_a = soup.find('a', href=True, string=True, target=True)
                 order_number = order_number_a.text
-            except AttributeError:
-                print('order number error 2')
-                order_number = []
 
-        try:
-            address_span = soup.find('span', style='font-family:helvetica;font-size:16px!important;font-weight:400!important;color:rgb(46,47,50)')
-            address_text = address_span.text
-        except AttributeError:
-            print('address error 1')
-            address_text = ''
-        if not address_text:
-            try:
-                address_div = soup.find('div',
-                                        style='color:#6d6e71;font-family:helvetica;font-size:16px;line-height:1.38;text-align:left;text-decoration:none;')
-                address_text = address_div.get_text(strip=True) if address_div else ''
-            except AttributeError:
-                print('address error 2, trying 3')
-                address_div = soup.find_all('p', style='margin:0')
-                if len(address_div) >= 3:
-                    address_text = address_div[2].get_text()
-                    print(address_text)
-                else:
-                    raise ValueError("There are not enough <p> elements in the HTML.")
+            return order_number
 
-        return tracking_numbers, order_number, order_date, address_text
+        def address_finder():
+            address_div = soup.find('div', style='color:#6d6e71;font-family:helvetica;font-size:16px;line-height:1.38;text-align:left;text-decoration:none;')
+            address = address_div.get_text(strip=True) if address_div else ''
+
+            if not address:
+                print('Address - Moving to method 2')
+                address_span = soup.find('span', style='font-family:helvetica;font-size:16px!important;font-weight:400!important;color:rgb(46,47,50)')
+                address = address_span.text
+
+                if not address:
+                    print('Address - Moving to method 3')
+                    address_div = soup.find_all('p', style='margin:0')
+                    if len(address_div) >= 3:
+                        address = address_div[2].get_text()
+                        print(address_text)
+                    else:
+                        print('Address - Method 3 failed, returning fail')
+                        address = 'Failed to retrieve address'
+
+            return address
+
+        return tracking_finder(), order_date_finder(), order_number_finder(), address_finder()
     except Exception as e:
         print(f"Error in extract_details: {e}")
         return [], '', '', ''
+
 
 def read_credentials(file_path):
     try:
@@ -82,6 +87,7 @@ def read_credentials(file_path):
         print(f"Error in read_credentials: {e}")
         raise
 
+
 try:
     imap_url = 'imap.gmail.com'
     file_path = 'credentials.txt'
@@ -94,13 +100,14 @@ try:
     start_date = datetime.date(2024, 1, 1)  # Year, Month, Day
     formatted_start_date = start_date.strftime("%d-%b-%Y")
 
-    status, messages = mail.search(None, f'(FROM "help@walmart.com" SUBJECT "Shipped" SINCE "{formatted_start_date}")')
-    messages = messages[0].split()
+    status, messages = mail.search(None,
+                                   f'(FROM "alertspoiler00.93@gmail.com" SUBJECT "Shipped" SINCE "{formatted_start_date}")')
+    messages = messages[0].split(b' ')
 
     with open('OrderDetails.csv', mode='w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
 
-        writer.writerow(['Tracking Number', 'Order Number', 'Order Date', 'Address'])
+        writer.writerow(['Tracking Number', 'Order Number', 'Order Date'])
 
         for mail_id in messages:
             status, data = mail.fetch(mail_id, '(RFC822)')
@@ -130,7 +137,7 @@ try:
 
     mail.close()
     mail.logout()
-except Exception as e:
+except:
     print(f"Unexpected error: {e}")
 
 input("Press Enter to exit.")
